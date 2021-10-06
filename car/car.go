36e7a4ad61b22b5c34d7c51605ef27e5c81b693e
Carl Riis <carltheperson@protonmail.com>
2021-10-06 23:30:45 +0200

    Speed slowing down when turning

--- car/car.go
+++ car/car.go
@@ -8,30 +8,33 @@ import (
 	"github.com/carltheperson/car-and-mouse/math"
 )
 
-const pxsPerMpf = 60
+const (
+	maxSpeed = 60
+	minSpeed = 40
 
-const maxDif = 0.09
+	maxTurningDif = 0.09
+)
 
 type Car struct {
-	x          int
-	y          int
-	lastX      int
-	lastY      int
-	width      int
-	height     int
-	direction  float64
-	correcting bool
+	x         int
+	y         int
+	lastX     int
+	lastY     int
+	width     int
+	height    int
+	direction float64
+	speed     float64
 }
 
 func NewCar(x int, y int) *Car {
 
 	return &Car{
-		x:          x,
-		y:          y,
-		width:      25,
-		height:     50,
-		direction:  0.0,
-		correcting: true,
+		x:         x,
+		y:         y,
+		width:     25,
+		height:    50,
+		direction: 0.0,
+		speed:     maxSpeed / 2,
 	}
 }
 
@@ -56,6 +59,10 @@ func (c *Car) Draw(ctx js.Value) {
 	ctx.Call("fill")
 }
 
+func (c *Car) ShouldDraw() bool {
+	return c.lastX != c.x || c.lastY != c.y
+}
+
 func (c *Car) Update(mouseX int, mouseY int, mpf float64) {
 	c.lastX = c.x
 	c.lastY = c.y
@@ -64,14 +71,21 @@ func (c *Car) Update(mouseX int, mouseY int, mpf float64) {
 	mouseRadians := math.ConvertDirectionVectorToRadians(mouseVector.GetUnitVector())
 
 	directionDifference := getDirectionDifference(c.direction, mouseRadians)
-	regulatedDirectedDifference := getRegulatedDirectionDifference(directionDifference, maxDif)
+	regulatedDirectedDifference := getRegulatedDirectionDifference(directionDifference, maxTurningDif)
+
+	turningFraction := stdMath.Abs(regulatedDirectedDifference) / maxTurningDif
+	if turningFraction > 0.5 {
+		c.speed -= (c.speed - minSpeed) * turningFraction * mpf * 0.4
+	} else {
+		c.speed += (maxSpeed - c.speed) * (1 - turningFraction) * mpf * 0.4
+	}
 
 	c.direction = c.direction - regulatedDirectedDifference*(mpf*10)
 	directionVector := math.ConvertRadiansToDirectionVector(c.direction)
 	directionUnitVector := directionVector.GetUnitVector()
 
-	c.x -= int(directionUnitVector.B * mpf * pxsPerMpf)
-	c.y += int(directionUnitVector.A * mpf * pxsPerMpf)
+	c.x -= int(directionUnitVector.B * mpf * c.speed)
+	c.y += int(directionUnitVector.A * mpf * c.speed)
 }
 
 // getRegulatedDirectionDifference will keep the difference to a max size
@@ -110,8 +124,4 @@ func getDirectionDifference(d1 float64, d2 float64) float64 {
 		return option2
 	}
 	return option3
-}
-
-func (c *Car) ShouldDraw() bool {
-	return c.lastX != c.x || c.lastY != c.y
 }
